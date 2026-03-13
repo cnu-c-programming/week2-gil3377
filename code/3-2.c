1f1421f4c3686d08c3592790b6903a57d18584a7
gil3377 <mungilj3@gmail.com>
2026-03-13 09:58:55 +0900

    Update 3-2.c

--- code/3-2.c
+++ code/3-2.c
@@ -2,6 +2,15 @@
 
 int main()
 {
+    int a = -1;
+    unsighned int b = 1;
+
+    if (a < b) {
+        printf("a < b\n" );
+    }
+    else {
+        printf("a >= b ???\n");
+    }
 
     return 0;
 }
