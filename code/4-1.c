7dd50e86db13354c3be2a9b3178d19263c2d52c9
gil3377 <mungilj3@gmail.com>
2026-03-13 22:08:06 +0900

    Update 4-1.c

--- code/4-1.c
+++ code/4-1.c
@@ -2,6 +2,23 @@
 
 int main()
 {
+    int a, b;
+    char c;
+
+    scanf("%d %d %c", &a, &b, &c);
+
+    if (c == '+'){
+        printf("%d\n", a + b);
+    }
+    else if (c == '-'){
+        printf("%d\n", a - b);
+    }
+    else if (c == '*'){
+        printf("%d\n", a * b);
+    }
+    else {
+        printf("%d\n", a / b);
+    }
 
     return 0;
 }
