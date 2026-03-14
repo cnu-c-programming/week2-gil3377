d1eb361ec4f46a4d78d1524d6eb6ea339ba86f5f
gil3377 <mungilj3@gmail.com>
2026-03-14 17:43:54 +0900

    Update 4-3.c

--- code/4-3.c
+++ code/4-3.c
@@ -2,7 +2,23 @@
 
 int main()
 {
+    int n, i;
 
+    scanf("%d", &n);
+
+    if (n <= 1) {
+        printf("false");
+        return 0;
+    }
+
+    for (i = 2; i < n; i++) {
+        if (n % i == 0) {
+            printf("false");
+            return 0;
+        }
+    }
+
+    printf("true");
     return 0;
 }
 
